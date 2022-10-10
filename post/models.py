37a1eb149bf7167164ec3cd1af3a9f8344d8d565
TwoEther <lhi0711@naver.com>
2022-10-10 17:05:24 +0900

    drone

--- post/models.py
+++ post/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class UserInfo(models.Model):
+class User(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
     passwd = models.CharField(max_length=20, blank=True, null=True)
     nickname = models.CharField(unique=True, max_length=20, blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'user_info'
+        db_table = 'user'
 
 class Category(models.Model):
     name = models.CharField(primary_key=True, max_length=20)
